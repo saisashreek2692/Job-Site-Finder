8023fc948f6848b46b2f23105ce3038b121c5bc5
Sai Sashreek <saisashreekd@gmail.com>
2024-02-08 10:52:48 +0530

    Added Content in Value Section

--- src/Components/Value/Value.jsx
+++ src/Components/Value/Value.jsx
@@ -1,7 +1,62 @@
+import menu from "../../Assets/menu.png";
+import pic from "../../Assets/Pic.ico";
+import pic1 from "../../Assets/1.jpg";
+
 const Value = () => {
   return (
-    <div>Value</div>
-  )
-}
+    <div className="mb-[4rem] mt-[6rem]">
+      <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block">
+        The value that holds us true and to account.
+      </h1>
 
-export default Value
+      <div className="grid gap-[10rem] grid-cols-3 items-center">
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+              <img src={menu} alt="value picture" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[13px] text-textColor opacity-[.7] py-[1rem]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit Asperiores,
+            illum quis repellat laboriosam
+          </p>
+        </div>
+
+        <div className="singleGrid rounded-[10px] hover:bg-[#f7edf5] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#f7d1e1] h-[40px] w-[40px] flex items-center justify-center">
+              <img src={pic} alt="value picture" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[13px] text-textColor opacity-[.7] py-[1rem]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit Asperiores,
+            illum quis repellat laboriosam
+          </p>
+        </div>
+
+        <div className="singleGrid rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#f3f2ad] h-[40px] w-[40px] flex items-center justify-center">
+              <img src={pic1} alt="value picture" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[13px] text-textColor opacity-[.7] py-[1rem]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit Asperiores,
+            illum quis repellat laboriosam
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Value;
